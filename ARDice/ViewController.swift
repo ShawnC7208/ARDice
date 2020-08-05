452337e567ae7b8a7425000f729bfde74ede5a8b
Shawn Chandwani <ShawnC7208@gmail.com>
2020-08-04 21:12:08 -0500

    Finish AR dice with roll animation, clear, and re roll

--- ARDice/ViewController.swift
+++ ARDice/ViewController.swift
@@ -13,6 +13,7 @@ import ARKit
 class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
+    var diceArray = [SCNNode]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -69,10 +70,45 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                     diceNode.position = SCNVector3(hitResult.worldTransform.columns.3.x,
                                                    hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
                                                    hitResult.worldTransform.columns.3.z)
+                    
+                    diceArray.append(diceNode)
                     sceneView.scene.rootNode.addChildNode(diceNode)
+                    roll(diceNode)
                 }
             }
         }
+    }
+    
+    @IBAction func removeAllDice(_ sender: UIBarButtonItem) {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                dice.removeFromParentNode()
+            }
+            diceArray.removeAll()
+        }
+    }
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice);
+            }
+        }
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    
+    func roll(_ node: SCNNode) {
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi / 2)
+        
+        node.runAction(SCNAction.rotateBy(x: CGFloat(randomX * 5), y: 0, z: CGFloat(randomZ * 5), duration: 0.5))
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
